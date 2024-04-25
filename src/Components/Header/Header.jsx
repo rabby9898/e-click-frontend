0ae891cdb2c71b10f5939555f807ccdeee0462ef
deltatranslator <deltatranslator16@gmail.com>
2024-04-25 15:16:13 +0600

    header basic  design added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,66 @@
+import { GrSearch } from "react-icons/gr";
+import { Link } from "react-router-dom";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+const Header = () => {
+  return (
+    <div className="h-16 shadow-md bg-white fixed w-full z-40">
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+        <div className="">
+          <img src="https://i.ibb.co/3mFM79K/eclick-logo.png" alt="" />
+        </div>
+
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
+          <input
+            type="text"
+            placeholder="search product here..."
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+            <GrSearch />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-7">
+          <div className="text-3xl cursor-pointer relative flex justify-center">
+            <FaRegCircleUser />
+          </div>
+
+          {/* <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
+            <nav>
+              <Link
+                to={"/admin-panel/all-products"}
+                className="whitespace-nowrap hidden md:block hover:bg-slate-100 p-2"
+              >
+                Admin Panel
+              </Link>
+            </nav>
+          </div> */}
+
+          <div>
+            <Link to={"/cart"} className="text-2xl relative">
+              <span>
+                <FaShoppingCart />
+              </span>
+
+              <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+                <p className="text-sm">0</p>
+              </div>
+            </Link>
+          </div>
+
+          <div>
+            <Link
+              to={"/login"}
+              className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
