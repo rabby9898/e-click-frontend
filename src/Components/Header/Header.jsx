29fc50b54a964ce3537bb762f3599803ab1937bb
deltatranslator <deltatranslator16@gmail.com>
2024-04-26 19:46:29 +0600

    logo design change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="h-20 shadow-md bg-white fixed w-full z-40">
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
         <div className="w-[80px] h-[80px] md:w-[100px] md:h-[100px] flex items-center">
-          <img src="https://i.ibb.co/3mFM79K/eclick-logo.png" alt="" />
+          <img src="https://i.ibb.co/z4XZBPB/e-click-logo.png" alt="" />
         </div>
 
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
