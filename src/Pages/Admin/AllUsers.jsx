cacdd4cf26ecddd5309c63a62f4092e535dddea6
rabby9898 <mdfajlerabby00@gmail.com>
2024-05-11 15:47:39 +0600

    allusers: table data added from backend

--- src/Pages/Admin/AllUsers.jsx
+++ src/Pages/Admin/AllUsers.jsx
@@ -1,23 +1,73 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SummaryApi from "../../common";
+import toast from "react-hot-toast";
 
-const AllUsers = async () => {
+const AllUsers = () => {
+  const [allUser, setAllUsers] = useState([]);
+
   const fetchAllUsers = async () => {
     const fetchData = await fetch(SummaryApi.allUsers.url, {
       method: SummaryApi.allUsers.method,
       credentials: "include",
     });
-    const data = await fetchData.json();
-    console.log("users Data", data);
+    const dataResponse = await fetchData.json();
+    if (dataResponse.success) {
+      setAllUsers(dataResponse.data);
+    }
+
+    if (dataResponse.error) {
+      toast.error(dataResponse.message);
+    }
   };
 
   useEffect(() => {
     fetchAllUsers();
   }, []);
   return (
-    <div>
-      <h1>All Users</h1>
-    </div>
+    <>
+      <div className="relative shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-2 py-3">
+                Sr.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Role
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Created Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Action</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUser.map((el, index) => {
+              return (
+                <tr
+                  key={el._id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <td className="px-6 py-4">{index + 1}</td>
+                  <td className="px-6 py-4">{el?.name}</td>
+                  <td className="px-6 py-4">{el?.email}</td>
+                  <td className="px-6 py-4">{el?.role}</td>
+                  <td className="px-6 py-4">{el?.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
