4df38d9fddee84671d0cf918e24a9b3768f5f5c3
rabby9898 <mdfajlerabby00@gmail.com>
2024-05-23 14:08:35 +0600

    search func added

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import SummaryApi from "../common";
+import { useLocation } from "react-router-dom";
+
 const Search = () => {
+  const query = useLocation();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProduct = async () => {
+    setLoading(true);
+    const response = await fetch(SummaryApi.searchProduct.url + query.search);
+    const dataResponse = await response.json();
+    setLoading(false);
+
+    setData(dataResponse.data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, [query]);
   return <div>Search</div>;
 };
 
