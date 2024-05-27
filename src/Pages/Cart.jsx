c0e49e08651bda727c2b42396e46a0c0434abd97
rabby9898 <mdfajlerabby00@gmail.com>
2024-05-27 08:25:04 +0600

    cart delivery method conditional added

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -3,11 +3,13 @@ import SummaryApi from "../common";
 import Context from "../Context/Context";
 import { MdDelete } from "react-icons/md";
 import PaymentForm from "../Components/CardForm/PaymentForm";
+import { useSelector } from "react-redux";
 const Cart = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const loadingCart = new Array(4).fill(null);
   const context = useContext(Context);
+  const user = useSelector((state) => state.user?.user);
 
   /***cart data fetching***/
   const fetchData = async () => {
@@ -193,63 +195,65 @@ const Cart = () => {
         {/***summary  */}
         <div className="mt-5 lg:mt-0 w-full max-w-sm h-auto">
           {/* delivery method */}
-          <div>
-            <p className="mt-8 text-lg font-medium">Shipping Methods</p>
-            <form className="mt-5 grid gap-6">
-              <div className="relative">
-                <input
-                  className="peer hidden"
-                  id="radio_1"
-                  type="radio"
-                  name="radio"
-                  checked
-                />
-                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-                <label
-                  className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                  for="radio_1"
-                >
-                  <img
-                    className="w-14 object-contain"
-                    src="https://i.ibb.co/YpTZL1G/hidden-arrow-fedex-logo.jpg"
-                    alt=""
+          {user && (
+            <div>
+              <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+              <form className="mt-5 grid gap-6">
+                <div className="relative">
+                  <input
+                    className="peer hidden"
+                    id="radio_1"
+                    type="radio"
+                    name="radio"
+                    checked
                   />
-                  <div className="ml-5">
-                    <span className="mt-2 font-semibold">Fedex Delivery</span>
-                    <p className="text-slate-500 text-sm leading-6">
-                      Delivery: 2-4 Days
-                    </p>
-                  </div>
-                </label>
-              </div>
-              <div className="relative">
-                <input
-                  className="peer hidden"
-                  id="radio_2"
-                  type="radio"
-                  name="radio"
-                  checked
-                />
-                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
-                <label
-                  className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                  for="radio_2"
-                >
-                  <img
-                    className="w-14 object-contain"
-                    src="https://i.ibb.co/0ZQfW0N/images.png"
-                    alt=""
+                  <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+                  <label
+                    className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                    for="radio_1"
+                  >
+                    <img
+                      className="w-14 object-contain"
+                      src="https://i.ibb.co/YpTZL1G/hidden-arrow-fedex-logo.jpg"
+                      alt=""
+                    />
+                    <div className="ml-5">
+                      <span className="mt-2 font-semibold">Fedex Delivery</span>
+                      <p className="text-slate-500 text-sm leading-6">
+                        Delivery: 2-4 Days
+                      </p>
+                    </div>
+                  </label>
+                </div>
+                <div className="relative">
+                  <input
+                    className="peer hidden"
+                    id="radio_2"
+                    type="radio"
+                    name="radio"
+                    checked
                   />
-                  <div className="ml-5">
-                    <span className="mt-2 font-semibold">DHL Delivery</span>
-                    <p className="text-slate-500 text-sm leading-6">
-                      Delivery: 2-4 Days
-                    </p>
-                  </div>
-                </label>
-              </div>
-            </form>
-          </div>
+                  <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+                  <label
+                    className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                    for="radio_2"
+                  >
+                    <img
+                      className="w-14 object-contain"
+                      src="https://i.ibb.co/0ZQfW0N/images.png"
+                      alt=""
+                    />
+                    <div className="ml-5">
+                      <span className="mt-2 font-semibold">DHL Delivery</span>
+                      <p className="text-slate-500 text-sm leading-6">
+                        Delivery: 2-4 Days
+                      </p>
+                    </div>
+                  </label>
+                </div>
+              </form>
+            </div>
+          )}
 
           {loading ? (
             <div className="h-36 bg-slate-200 border border-slate-300 animate-pulse"></div>
